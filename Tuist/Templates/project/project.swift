9e9d349cbf35c9487b1c5c677ab22625f7392d7f
Thimo Bess <thimo.bess@gmail.com>
2022-03-22 08:17:47 +0100

    Add Stencil files

--- Tuist/Templates/project/project.swift
+++ Tuist/Templates/project/project.swift
@@ -10,6 +10,13 @@ import ProjectDescription
 let projectNameAttribute: Template.Attribute = .required("name")
 let devTeamAttribute: Template.Attribute = .required("devTeam")
 let bundleIdentifierPrefixAttribute: Template.Attribute = .required("bundlePrefix")
+// let platformAttribute: Template.Attribute = .optional("platform", default: "iOS")
+
+let appPath = "Targets/\(projectNameAttribute)"
+let projectPath = "."
+func templatePath(_ path: String) -> Path {
+    "Stencils/\(path)"
+}
 
 let template = Template(
     description: "Tuist Project Template",
@@ -17,14 +24,45 @@ let template = Template(
         projectNameAttribute,
         devTeamAttribute,
         bundleIdentifierPrefixAttribute,
+        // platformAttribute,
         .optional("organization", default: ""),
-        .optional("platform", default: "ios"),
     ],
     items: [
-        .file(path: "Project.swift", templatePath: "Stencils/project.stencil"),
-        .directory(path: "Targets/{{ project }}/Sources", sourcePath: "Stencils/Sources"),
-        .directory(path: "Targets/{{ project }}/Resources", sourcePath: "Stencils/Resources"),
-        .directory(path: "Targets/{{ project }}/Tests", sourcePath: "Stencils/Tests"),
-        .directory(path: "Targets/{{ project }}/UITests", sourcePath: "Stencils/UITests"),
+        .file(path: projectPath + "/Project.swift", 
+              templatePath: templatePath("project.stencil")
+        ),
+        .file(
+            path: ".gitignore",
+            templatePath: templatePath("gitignore.stencil")
+        ),
+
+        //App Files
+        .file(
+            path: appPath + "/Sources/\(projectNameAttribute)App.swift",
+            templatePath: templatePath("app.stencil")
+        ),
+        .directory(
+                path: appPath + "/Resources",
+                sourcePath: "ios/Assets.xcassets"
+            ),
+        .directory(
+                path: appPath + "/Resources",
+                sourcePath: "Preview Content"
+            ),
+
+        //Tuist Files
+        .file(
+            path: "Tuist/Config.swift",
+            templatePath: templatePath("Config.stencil")
+        ),
+        .file(
+            path: "Tuist/Dependencies.swift",
+            templatePath: templatePath("dependencies.stencil")
+        ),
+
+        // .directory(path: "Targets/{{ project }}/Sources", sourcePath: "Stencils/Sources"),
+        // .directory(path: "Targets/{{ project }}/Resources", sourcePath: "Stencils/Resources"),
+        // .directory(path: "Targets/{{ project }}/Tests", sourcePath: "Stencils/Tests"),
+        // .directory(path: "Targets/{{ project }}/UITests", sourcePath: "Stencils/UITests"),
     ]
 )
